dd8cc329171746170f730ea672a45aa4d2e06424
maheem_20 <mwistanbuly@gmail.com>
2023-07-04 22:52:37 +0200

    add middleware function to set up CORS headers to allow cross-origin requests

--- backend/index.js
+++ backend/index.js
@@ -6,4 +6,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
