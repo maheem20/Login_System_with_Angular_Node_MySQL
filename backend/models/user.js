fd782103c60983467ff0306a39c1d710a82c3c5b
maheem_20 <mwistanbuly@gmail.com>
2023-07-05 23:59:30 +0200

    add constructor for User class

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,9 @@
 const db = require('../util/database');
 
-module.exports = class User { };
+module.exports = class User {
+    constructor(name, email, password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+};
