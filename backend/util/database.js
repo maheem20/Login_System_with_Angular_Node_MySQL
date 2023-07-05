1e15e38825463a855ac466b9df30605486ba4450
maheem_20 <mwistanbuly@gmail.com>
2023-07-05 23:49:12 +0200

    create connection pool
    pool object returned is used to execute queries against database

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,2 +1,9 @@
 const mysql = require('mysql2');
 const config = require('../config/config.json');
+
+const pool = mysql.createPool({
+    host: config.host,
+    user: config.user,
+    password: config.database,
+    database: config.password
+});
