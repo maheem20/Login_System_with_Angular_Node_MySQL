01e7f5203de6c5d2fff3d21132f3475e584fef23
maheem_20 <mwistanbuly@gmail.com>
2023-07-06 23:48:33 +0200

    handle POST requests to /signup route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,3 +2,5 @@ const express = require('express');
 const { body } = require('express-validator/check');
 const router = express.Router();
 const User = require('../models/user');
+
+router.post('/signup');
