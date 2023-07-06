b9326935e92bda2852723d024d624f5fc44ab349
maheem_20 <mwistanbuly@gmail.com>
2023-07-06 23:50:51 +0200

    validate request body using Express-validator
    name field must not be empty
    
    email field must be valid email address

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,4 +3,10 @@ const { body } = require('express-validator/check');
 const router = express.Router();
 const User = require('../models/user');
 
-router.post('/signup');
+router.post(
+    '/signup',
+    [
+        body('name').trim().not().isEmpty(),
+        body('email').isEmail().withMessage('Please enter a valid email.')
+    ]
+);
