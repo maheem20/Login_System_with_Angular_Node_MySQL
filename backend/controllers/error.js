07eb3283f2ed10779cfd5aa67589c20601b67c6f
maheem_20 <mwistanbuly@gmail.com>
2023-08-27 23:55:44 +0200

    set error message to message of error object

--- backend/controllers/error.js
+++ backend/controllers/error.js
@@ -7,4 +7,9 @@ exports.get404 = (req, res, next) => {
 exports.get500 = (error, req, res, next) => {
     const data = error.data;
     res.status(error.statusCode || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
 };
