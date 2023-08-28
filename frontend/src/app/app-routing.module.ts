f13dc79a66e448b1e2c8933a46feb1cc855e1fe3
maheem_20 <mwistanbuly@gmail.com>
2023-08-28 23:39:03 +0200

    define catch-all route that redirects to HomeComponent

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'posts', component: PostsComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
