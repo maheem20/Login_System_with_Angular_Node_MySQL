00ed75682a1153796f705640e3199f175687eb34
maheem_20 <mwistanbuly@gmail.com>
2023-08-28 23:34:04 +0200

    import necessary components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component'; // Add this
+import { PostsComponent } from './components/posts/posts.component'; // Add this
+import { LoginComponent } from './components/login/login.component'; // Add this
+import { SignupComponent } from './components/signup/signup.component'; // Add this
+
 const routes: Routes = [];
 
 @NgModule({
