1511911cbd10f2b378a1fcca748cb23f162285ca
maheem_20 <mwistanbuly@gmail.com>
2023-08-28 23:22:14 +0200

    create navigation component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,10 +10,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
