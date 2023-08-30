eba33acd88996b6206ac21273156247ed4e2aac5
maheem_20 <mwistanbuly@gmail.com>
2023-08-30 22:39:15 +0200

    import HttpClient, HttpHeaders & User

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
