c4e484ffb0c670850b1e6ea8a471043d0068e962
maheem_20 <mwistanbuly@gmail.com>
2023-09-02 23:46:04 +0200

    handle POST requests to /post URL validate the title, body & user fields

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 const postsController = require('../controllers/posts');
 const router = express.Router();
 
 router.get('/', postsController.fetchAll);
+
+router.post(
+    '/post',
+    [
+        body('title').trim().isLength({ min: 5 }).not().isEmpty(),
+        body('body').trim().not().isLength({ min: 10 }).isEmpty(),
+        body('user').trim().not().isEmpty()
+    ], postsController.postPost
+);
