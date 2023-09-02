b966516a3bdf1571693dc7b2b6b9309804d2bea0
maheem_20 <mwistanbuly@gmail.com>
2023-09-02 22:07:18 +0200

    inject Router service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { BehaviorSubject, Observable } from 'rxjs';
 import { first, catchError, tap } from 'rxjs/operators';
@@ -20,7 +21,11 @@ export class AuthService {
     headers: new HttpHeaders({ "Content-Type": "application/json" })
   };
 
-  constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) { }
+  constructor(
+    private http: HttpClient,
+    private errorHandlerService: ErrorHandlerService,
+    private router: Router
+  ) { }
 
   signup(user: Omit<User, "id">): Observable<User> {
     return this.http.post<User>(`${this.url}/signup`, user, this.httpOptions).pipe(
