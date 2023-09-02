258a0491b2153d452a1291be7c4dacae9087648a
maheem_20 <mwistanbuly@gmail.com>
2023-09-02 23:19:04 +0200

    remove line added by mistake

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const authRoutes = require('./routes/auth');
-const e = require('express');
 
 const app = express();
 
