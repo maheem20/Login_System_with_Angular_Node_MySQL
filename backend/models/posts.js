d3093b11928dcd03723c6b71740516f8f7192f57
maheem_20 <mwistanbuly@gmail.com>
2023-09-03 16:14:51 +0200

    define Post class

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -1,1 +1,9 @@
 const db = require('../util/database');
+
+module.exports = class Post {
+    constructor(title, body, user) {
+        this.title = title;
+        this.body = body;
+        this.user = user;
+    }
+};
