98693f8fc7ab8ccbd983ade819aa321cbf97c3bc
maheem_20 <mwistanbuly@gmail.com>
2023-09-03 22:19:44 +0200

    send JSON response with fetched posts

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -4,5 +4,6 @@ const Post = require('../models/post');
 exports.fetchAll = async (req, res, next) => {
     try {
         const [allPosts] = await Post.fetchAll();
+        res.status(200).json(allPosts);
     }
 };
