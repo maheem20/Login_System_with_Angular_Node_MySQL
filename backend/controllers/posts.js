db2d854a35b336408d7c3d2b17268c81f5797337
maheem_20 <mwistanbuly@gmail.com>
2023-09-03 22:20:04 +0200

    handle errors

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -5,5 +5,10 @@ exports.fetchAll = async (req, res, next) => {
     try {
         const [allPosts] = await Post.fetchAll();
         res.status(200).json(allPosts);
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
     }
 };
