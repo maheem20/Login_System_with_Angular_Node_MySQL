62a9c941ce5402b59c4edcede8c9dbe2db62f64e
maheem_20 <mwistanbuly@gmail.com>
2023-09-04 21:36:14 +0200

    if no Authorization header is present, throw error

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => { }
+module.exports = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+        const error = new Error('Not authenticated!');
+        error.statusCode = 401;
+        throw error;
+    }
+}
