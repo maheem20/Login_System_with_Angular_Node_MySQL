94ac90c6d19e271999ee581e6fffdec727ac4b4d
maheem_20 <mwistanbuly@gmail.com>
2023-09-04 21:53:40 +0200

    use authentication middleware

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const { body } = require('express-validator/check');
 const postsController = require('../controllers/posts');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/', postsController.fetchAll);
+router.get('/', auth, postsController.fetchAll);
 
 router.post(
     '/post',
     [
+        auth,
         body('title').trim().isLength({ min: 5 }).not().isEmpty(),
         body('body').trim().not().isLength({ min: 10 }).isEmpty(),
         body('user').trim().not().isEmpty()
     ], postsController.postPost
 );
 
-router.delete('/:id', postsController.deletePost);
+router.delete('/:id', auth, postsController.deletePost);
 
 module.exports = router;
