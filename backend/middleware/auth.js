bc46b52077e8484268105b18b78526c7805a96ea
maheem_20 <mwistanbuly@gmail.com>
2023-09-04 21:46:30 +0200

    if token is valid but decoded token is null throw error

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -15,4 +15,9 @@ module.exports = (req, res, next) => {
         err.statusCode = 500;
         throw err;
     }
+    if (!decodedToken) {
+        const error = new Error('Not authenticated!');
+        error.statusCode = 401;
+        throw error;
+    }
 };
