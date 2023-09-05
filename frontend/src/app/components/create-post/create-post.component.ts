6fac8eefc031426a7eb6c3d73059be421a263025
maheem_20 <mwistanbuly@gmail.com>
2023-09-05 22:28:51 +0200

    define method called "createFormGroup" that returns new FormGroup object

--- frontend/src/app/components/create-post/create-post.component.ts
+++ frontend/src/app/components/create-post/create-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class CreatePostComponent {
   form: FormGroup | undefined;
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({});
+  }
 }
