26b7ed7ac8a504cf34bf69c2074b9512bcff72dc
maheem_20 <mwistanbuly@gmail.com>
2023-09-06 23:53:41 +0200

    define User object that only contains id property

--- frontend/src/app/components/posts/posts.component.ts
+++ frontend/src/app/components/posts/posts.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Post } from 'src/app/models/Post';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent  {
+export class PostsComponent {
   posts$!: Observable<Post[]>;
+  userId!: Pick<User, "id">;
 }
