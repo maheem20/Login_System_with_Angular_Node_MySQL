cc2abf63ee2744dd5f110f1dadb182a413cb8621
maheem_20 <mwistanbuly@gmail.com>
2023-09-06 23:45:09 +0200

    define HTTP headers for requests

--- frontend/src/app/services/post.service.ts
+++ frontend/src/app/services/post.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +9,9 @@ import { Injectable } from '@angular/core';
 export class PostService {
   private url = 'http://localhost:3000/posts';
 
-  constructor() { }
+  httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" })
+  };
+
+  constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) { }
 }
