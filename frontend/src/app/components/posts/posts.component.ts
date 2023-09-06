95b1ef0bcc90256ef633b8d1578b4accde25d6c7
maheem_20 <mwistanbuly@gmail.com>
2023-09-06 23:51:05 +0200

    define observable that emits array of Post objects

--- frontend/src/app/components/posts/posts.component.ts
+++ frontend/src/app/components/posts/posts.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { Post } from 'src/app/models/Post';
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-
+export class PostsComponent  {
+  posts$!: Observable<Post[]>;
 }
