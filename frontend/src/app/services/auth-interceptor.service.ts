4255b08155729eae85ef0892985c275ccda2d03a
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-07 21:25:29 +0000

    implement HttpInterceptor interface

--- frontend/src/app/services/auth-interceptor.service.ts
+++ frontend/src/app/services/auth-interceptor.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptorService {
-
-  constructor() { }
+export class AuthInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  }
 }
