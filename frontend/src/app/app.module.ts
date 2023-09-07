176139080e9c4c173fb3034fda7dedd2963a9131
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-07 21:34:46 +0000

    register AuthInterceptorService as HTTP_INTERCEPTOR provider with Angular dependency injection system

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +18,8 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { CreatePostComponent } from './components/create-post/create-post.component';
+
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -41,7 +43,10 @@ import { CreatePostComponent } from './components/create-post/create-post.compon
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptorService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
